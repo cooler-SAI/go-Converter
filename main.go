55779bf9fa2ce6589dfe9e1c52dfba00bcb05d71
cooler-SAI <coolerme86@gmail.com>
2025-05-23 19:48:09 +0300

    feat: Implement basic currency conversion logic
    This commit introduces the initial structure for a currency converter.
    
    - Defines a `hardcodedRates` map to store predefined exchange rates between USD, EUR, and RUB.
    - Implements a `convertCurrency` function that takes an amount, source currency, and target currency, and returns the converted amount or an error if the conversion rate is not found.
    - The `main` function currently prints a welcome message, with further input handling to be implemented.

--- main.go
+++ main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
+
+var hardcodedRates = map[string]float64{
+	"USD_EUR": 0.92, // 1 USD = 0.92 EUR
+	"EUR_USD": 1.08, // 1 EUR = 1.08 USD
+	"USD_RUB": 90.0, // 1 USD = 90.0 RUB
+	"RUB_USD": 1.0 / 90.0,
+	"EUR_RUB": 98.0, // 1 EUR = 98.0 RUB
+	"RUB_EUR": 1.0 / 98.0,
+}
+
+func convertCurrency(amount float64, fromCurrency string, toCurrency string) (float64, error) {
+	fromCurrency = strings.ToUpper(fromCurrency)
+	toCurrency = strings.ToUpper(toCurrency)
+
+	if fromCurrency == toCurrency {
+		return amount, nil
+	}
+
+	// Hardcoded
+	rateKey := fmt.Sprintf("%s_%s", fromCurrency, toCurrency)
+	if rate, ok := hardcodedRates[rateKey]; ok {
+		return amount * rate, nil
+	}
+	return 0, fmt.Errorf("can't convert %s to %s: cource doesn't found", fromCurrency, toCurrency)
+}
 
 func main() {
-	fmt.Println("Hello World")
+	fmt.Println("Hello! Please enter data in format: " +
+		"")
 }
